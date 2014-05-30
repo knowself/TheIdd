b7193c661e6cea1b404309f6cada768726a57206
Joe Terry <joe@joeterry.org>
2014-05-30 13:06:12 -0700

    first changes and pushes to heroku

--- src/main/scala/HerokuApp.scala
+++ src/main/scala/HerokuApp.scala
@@ -7,11 +7,7 @@ class HerokuApp extends ScalatraServlet {
   get("/") {
     <html>
       <body>
-        <h1>This is
-          <a href="http://scalatra.org/2.2/guides/deployment/heroku.html">
-            scalatra-heroku
-          </a>!
-        </h1>
+        <h1>TheIdd is a Scalatra App!</h1>
       </body>
     </html>
   }
