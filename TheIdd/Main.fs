187e7d0b6ad37a583211a654679413df8a0b0977
Joe Terry <Joe@JoeTerry.org>
2014-12-18 00:35:32 -0800

    getting use to the environment and tools

--- TheIdd/Main.fs
+++ TheIdd/Main.fs
@@ -51,7 +51,7 @@ module Site =
         ]
 
     let HomePage =
-        Skin.WithTemplate "JoePage" <| fun ctx ->
+        Skin.WithTemplate "TheIdd: Intelligence Amplifier" <| fun ctx ->
             [
                 Div [Text "HOME"]
                 Div [new Controls.EntryPoint()]
